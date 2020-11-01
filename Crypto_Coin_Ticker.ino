926a09036501df9f152461739f60089e8b88aa16
frittna <60677128+frittna@users.noreply.github.com>
2020-11-01 13:43:52 +0100

    Update Crypto_Coin_Ticker.ino

--- Crypto_Coin_Ticker.ino
+++ Crypto_Coin_Ticker.ino
@@ -19,7 +19,7 @@
 // Menu Loader compatible, if SD-Updater (menu.bin) is installed in your SD-card hold buttonA while booting up to start MenuLoader to load your apps
 // the impoovements are based quick and dirty solutions - no complains please ;) - changings welcome :)
 // known bugs: buttonC often has the bug that it reacts like if it was pressed twice.. The battery symbol could be more precise,
-//             if you have power-on/off issues because you use a different battery module see "//  Setting Power:" in the code
+//             if you have power-on/off issues because you use a different battery module see "// Setting Power:" in the code
 
 // INSTALLATION INSTRUCTIONS:
 // - download Arduino IDE from their homepage https://www.arduino.cc/en/Main/Software
